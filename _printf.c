02ff749bdd93d30ae6d61e68fcdfaf77c6c3427c
audreyotani <nicoleaudreyotani@gmail.com>
2022-10-20 02:48:39 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -1,49 +1,20 @@
 #include "main.h"
 
 /**
- *_printf - Print a formatted string
- *@format: format string
- *Return: number of characters printed
+ *_printf - prints to the output
+ * @format: pointer
+ * Return: input length
  */
+
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list list;
-	char *pointer, *start;
-	param_func flags = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	va_list arg;
+	int done;
 
-	va_start(list, format);
+	va_start(arg, format);
+	done = vfprintf(stdout, format, arg);
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (pointer = (char *)format; *pointer; pointer++)
-	{
-		init_params(&flags, list);
-		if (*pointer != '%')
-		{
-			count += _putchar(*pointer);
-			continue;
-		}
-		start = pointer;
-		pointer++;
-		while (get_flags(pointer, &flags))
-		{
-			pointer++;
-		}
-		pointer = get_width(pointer, &flags, list);
-		pointer = get_precision(pointer, &flags, list);
-		if (get_mods(pointer, &flags))
-			pointer++;
+	va_end(arg);
 
-		if (!func_parse(pointer))
-			count += print_range(start, pointer,
-			flags.l_mod || flags.h_mod ? pointer - 1 : 0);
-		else
-			count += print_func(pointer, list, &flags);
-	}
-	_putchar(-1);
-	va_end(list);
-	return (count);
+	return (done);
 }
